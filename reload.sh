319c1579124b2228d20f5e7609434b00969127d8
1 <ressiwage@ya.ru>
2023-10-02 18:00:37 +0500

    new scripts

--- reload.sh
+++ reload.sh
@@ -1,8 +1,8 @@
-sh install.sh
+docker-compose down
+docker rm db
+docker stop py
+docker rm py
 
 docker-compose up -d
-
-docker exec -i db mysql -u root -pr3Dk7jcPBsSNtoTYxhGX library < initial.sql
-
 docker build -t py_i . --no-cache
 docker run --name py --expose=4000 -p 4000:5000 -d py_i:latest
