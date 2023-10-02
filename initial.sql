4d3d2bbaf14375341f2c1a132d48158302d04f79
1 <ressiwage@ya.ru>
2023-10-02 18:39:58 +0500

    sql

--- initial.sql
+++ initial.sql
@@ -1,21 +1,21 @@
-CREATE TABLE `library`.`users` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(250),
-  PRIMARY KEY (`id`));
+-- CREATE TABLE `library`.`users` (
+--   `id` INT NOT NULL AUTO_INCREMENT,
+--   `name` VARCHAR(250),
+--   PRIMARY KEY (`id`));
 
-CREATE TABLE `library`.`books` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(250),
-  `author` VARCHAR(250),
-  `genre` VARCHAR(250),
-  PRIMARY KEY (`id`));
+-- CREATE TABLE `library`.`books` (
+--   `id` INT NOT NULL AUTO_INCREMENT,
+--   `name` VARCHAR(250),
+--   `author` VARCHAR(250),
+--   `genre` VARCHAR(250),
+--   PRIMARY KEY (`id`));
 
 CREATE TABLE `library`.`r_u_b` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `user_id` INT NOT NULL,
   `book_id` INT NOT NULL,
-  `take_date` DATE NOT NULL DEFAULT CURRENT_DATE,
-  `return_date` DATE,
+  `take_ts` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `return_ts` TIMESTAMP,
   PRIMARY KEY (`id`));
 
 USE library;
