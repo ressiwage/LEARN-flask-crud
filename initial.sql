18bef13e8ba7e071d577ba3f4053168a588d6ed3
1 <ressiwage@ya.ru>
2023-10-02 19:29:01 +0500

    fix

--- initial.sql
+++ initial.sql
@@ -21,4 +21,4 @@ CREATE TABLE `library`.`r_u_b` (
 USE library;
 INSERT INTO users (name) VALUES ('john'), ('matthew'), ('karrie');
 INSERT INTO books (name, author, genre) VALUES ('antifragility', 'john doe', 'psychology'), ('python for beginners', 'conan doyle', 'technical'), ('mysql for beginners','conan doyle', 'technical');
-INSERT INTO r_u_b (user_id, book_id) VALUES (1, 1), (2,2), (3,2);
+INSERT INTO r_u_b (user_id, book_id) VALUES (1, 1), (2,2), (2,3);
--- python/main.py
+++ python/main.py
@@ -19,7 +19,7 @@ def index():
     books = tables['library']['books']
     rel = tables['library']['r_u_b']
     with Session(engines['library']) as session:
-        users = [dict(u._mapping) for u in session.execute(
+        users_books = [dict(u._mapping) for u in session.execute(
             select(
             *[column.label(str(column)) for column in users.c],
             *[column.label(str(column)) for column in books.c]
@@ -28,10 +28,13 @@ def index():
             .join(rel, rel.c.user_id==users.c.id, isouter=True)
             .join(books, rel.c.book_id==books.c.id, isouter=True))
             ).all()]
-    print(users)
-    for k,v in groupby(sorted(users, key=lambda x:x['users.id']),key=lambda x:x['users.id']):
-        print(k, list(v))
-        
+        users = [dict(u._mapping) for u in session.execute(select(users.c).select_from(users).all())]
+    print(users_books)
+    u_b = {}
+    for k,v in groupby(sorted(users_books, key=lambda x:x['users.id']),key=lambda x:x['users.id']):
+        u_b[k] = [{i:j for i,j in book.items() if 'book.' in i} for book in list(v)]
+    for i,_ in enumerate(users):
+        users[i]['books'] = u_b[users[i]['id']]
     return render_template('index.html', users=users)
         
 
