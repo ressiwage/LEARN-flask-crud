bf0d5914f5dd5bf0c0ddd4088f9040823c068f78
1 <ressiwage@ya.ru>
2023-10-02 16:55:49 +0500

    test installing

--- install.sh
+++ install.sh
@@ -7,5 +7,8 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 apt-cache policy docker-ce
 sudo apt install docker-ce
 
+docker pull mysql/mysql-server:8.0.30
 
-sudo docker pull mysql/mysql-server:8.0.30
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
