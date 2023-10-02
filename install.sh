7f8143ecafdfd31fb1e7aa3b7831345604e9d7cd
1 <ressiwage@ya.ru>
2023-10-02 17:06:10 +0500

    test installing

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
-# Add Docker's official GPG key:
-sudo apt update
+sudo apt install gnome-terminal
+sudo apt remove docker-desktop
+rm -r $HOME/.docker/desktop
+sudo rm /usr/local/bin/com.docker.cli
+sudo apt purge docker-desktop
 sudo apt-get update
+sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 sudo apt install docker-ce
+sudo systemctl status docker
+
 
 docker pull mysql/mysql-server:8.0.30
 
