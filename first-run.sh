436fc0e879d0a0a4cefce9bd2dd7b7549226eb4d
1 <ressiwage@ya.ru>
2023-10-02 18:45:46 +0500

    fix

--- first-run.sh
+++ first-run.sh
@@ -4,5 +4,5 @@ docker-compose up -d
 
 docker exec -i db mysql -u root -pr3Dk7jcPBsSNtoTYxhGX library < initial.sql
 
-docker build -t py_i . --no-cache
+docker build --network=host -t py_i . --no-cache
 docker run --name py --expose=4000 -p 4000:5000 -d py_i:latest
--- reload.sh
+++ reload.sh
@@ -4,5 +4,5 @@ docker stop py
 docker rm py
 
 docker-compose up -d
-docker build -t py_i . --no-cache
+docker build --network=host -t py_i . --no-cache
 docker run --name py --expose=4000 -p 4000:5000 -d py_i:latest
