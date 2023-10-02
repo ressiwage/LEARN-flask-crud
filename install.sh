1c1abd49b02accc7dc901dc345e025c22ea43a06
1 <ressiwage@ya.ru>
2023-10-02 17:49:10 +0500

    test installing

--- install.sh
+++ install.sh
@@ -9,11 +9,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 sudo apt install docker-ce
-sudo systemctl status docker
 
 
-docker pull mysql/mysql-server:8.0.30
-
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo apt-get update
+sudo apt-get install docker-compose-plugin
