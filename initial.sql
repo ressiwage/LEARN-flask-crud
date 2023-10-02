1bcf69025fad39d287a5cb6279ebe1a1b839b74c
1 <ressiwage@ya.ru>
2023-10-02 18:42:32 +0500

    fix

--- initial.sql
+++ initial.sql
@@ -1,14 +1,14 @@
--- CREATE TABLE `library`.`users` (
---   `id` INT NOT NULL AUTO_INCREMENT,
---   `name` VARCHAR(250),
---   PRIMARY KEY (`id`));
+CREATE TABLE `library`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(250),
+  PRIMARY KEY (`id`));
 
--- CREATE TABLE `library`.`books` (
---   `id` INT NOT NULL AUTO_INCREMENT,
---   `name` VARCHAR(250),
---   `author` VARCHAR(250),
---   `genre` VARCHAR(250),
---   PRIMARY KEY (`id`));
+CREATE TABLE `library`.`books` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(250),
+  `author` VARCHAR(250),
+  `genre` VARCHAR(250),
+  PRIMARY KEY (`id`));
 
 CREATE TABLE `library`.`r_u_b` (
   `id` INT NOT NULL AUTO_INCREMENT,
